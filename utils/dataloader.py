aa6a9c1ea739d8e19e881c38b29cb14e9f702ee8
bohua12 <bohua12@gmail.com>
2025-04-08 22:00:28 +0800

    Removed bloat from dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 
 def collate_identity(batch):
-    # Allows us to return np.array for __getitem__, instead of tensor
+    # Allows us to return np.array for __getitem__, instead of tensor (not used anymore)
     return tuple(np.stack(t) for t in zip(*batch))
 
 
@@ -80,87 +80,9 @@ def data_partition(fname, fraw, args):
         n_users, n_items_m, n_items_a, n_items_b
     )
 
-"""Simple, split into train/test/valid"""
-def data_partition_old(fname):
-    usernum = 0
-    itemnum = 0
-    User = defaultdict(list)
-    user_train = {}
-    user_valid = {}
-    user_test = {}
-    # assume user/item index starting from 1
-    f = open('data/%s.txt' % fname, 'r')
-
-    ## Store interaction in User Dict
-    for line in f:
-        u, i = line.rstrip().split(' ')
-        u = int(u)
-        i = int(i)
-        usernum = max(u, usernum)
-        itemnum = max(i, itemnum)
-        User[u].append(i)
-
-    for user in User:
-        nfeedback = len(User[user])
-        if nfeedback < 3:
-            user_train[user] = User[user]
-            user_valid[user] = []
-            user_test[user] = []
-        else:
-            user_train[user] = User[user][:-2]
-            user_valid[user] = []
-            user_valid[user].append(User[user][-2])
-            user_test[user] = []
-            user_test[user].append(User[user][-1])
-    return [user_train, user_valid, user_test, usernum, itemnum]
-
-def get_dataloader_old(user_train, usernum, itemnum, args):
-    ds = SASRecDataset(user_train, usernum, itemnum, args.maxlen)
-    return DataLoader(ds, batch_size=args.batch_size, shuffle=True, num_workers=0)
-
 def get_dataloader(train_m, train_a, train_b, usernum, itemnum_m, itemnum_a, itemnum_b, args):
     ds = CDSRDataset(train_m, train_a, train_b, usernum, itemnum_m, itemnum_a, itemnum_b, args)
-    return DataLoader(ds, batch_size=args.batch_size, shuffle=True, num_workers=0,)
-
-""" Standard implementation of abstract class Dataset, to be instantiated and used with DataLoader class"""
-class SASRecDataset(Dataset):
-    def __init__(self, user_train, usernum, itemnum, maxlen):
-        self.user_train = user_train
-        self.usernum = usernum
-        self.itemnum = itemnum
-        self.maxlen = maxlen
-        self.users = list(user_train.keys())
-    
-    def __len__(self):
-        return len(self.users)
-    
-    def __getitem__(self, idx):
-        # Gets uid of current indexed user
-        uid = self.users[idx]
-        # Initialise empty arrays of len(maxlen)
-        seq = np.zeros([self.maxlen], dtype=np.int32)
-        pos = np.zeros([self.maxlen], dtype=np.int32)
-        neg = np.zeros([self.maxlen], dtype=np.int32) # Randomly sampled incorrect item (-ve sample)
-        nxt = self.user_train[uid][-1]
-        idx = self.maxlen - 1
-
-        # Set of ALL items users[idx] have interacted with
-        ts = set(self.user_train[uid])
-        # Reason1 it is reversed: 0-Padding concept
-        # Reason2 it is reversed: Fill in rightmost, latest ones first, and earliest interactions past maxLen are dropped
-        for i in reversed(self.user_train[uid][:-1]):
-            seq[idx] = i
-            pos[idx] = nxt
-            # As long as "nxt" is a valid item (ie. not "0" padding), we can generate a -ve sample 
-            if nxt != 0:
-                neg[idx] = random_neq(1, self.itemnum + 1, ts) #random_neq generates a random -ve sample user never inr with
-            nxt = i
-            idx -= 1
-            if idx == -1:
-                break
-
-        return uid, seq, pos, neg
-    
+    return DataLoader(ds, batch_size=args.batch_size, shuffle=True, num_workers=0,)    
 
 class CDSRDataset(Dataset):
     def __init__(self, train_m, train_a, train_b, usernum, itemnum_m, itemnum_a, itemnum_b, args):
