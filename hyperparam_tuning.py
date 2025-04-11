7cd8d14f01ee76377ae6eca055728fce61552628
bohua12 <bohua12@gmail.com>
2025-04-12 04:53:29 +0800

    Update hyperparam_tuning.py

--- hyperparam_tuning.py
+++ hyperparam_tuning.py
@@ -11,7 +11,7 @@ import time
 
 
 # Define hyperparameter grid search space
-hidden_units = [32, 64, 128] 
+hidden_units = [32] 
 lrs = [0.001, 0.0005, 0.0001] # Dont try 0.01
 dropout_rates = [0.1, 0.2, 0.3, 0.4] # Sometimes even up to 0.9
 weight_decays = [1e-4, 1e-3, 1e-2]
@@ -48,7 +48,7 @@ os.environ['PYTHONHASHSEED'] = str(args.seed)
 itr = 1
 t_start = 0
 t_end = 0
-with open("best_params_100epochs.txt", "w") as f:
+with open("tuning_3.txt", "w") as f:
     for hidden, lr, dropout, weight_decay in itertools.product(hidden_units, lrs, dropout_rates, weight_decays):
         start_time = time.time()  # Start timing the iteration
 
@@ -80,6 +80,7 @@ with open("best_params_100epochs.txt", "w") as f:
         print(f"Time for iteration {itr}: {end_time - start_time:.2f} seconds")
         f.write(f"Time for iteration {itr}: {end_time - start_time:.2f} seconds\n")
         f.write("\n")
+        f.flush()
         itr += 1
                                     
     f.write(f"Best OVERALL: {best_params}\n")
