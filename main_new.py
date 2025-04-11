8ab09a7677008bb8c1d3431ff83bf87634d82cff
bohua12 <bohua12@gmail.com>
2025-04-11 18:30:34 +0800

    Update main_new.py

--- main_new.py
+++ main_new.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     trainer = Trainer(args)
 
     epoch_start_idx = 1
-    best_m_valid_ndcg, best_a_valid_ndcg, best_b_valid_ndcg = 0.0, 0.0, 0,0
+    best_m_valid_ndcg, best_a_valid_ndcg, best_b_valid_ndcg = 0.0, 0.0, 0.0
 
 
     # SET UP LOGGING
