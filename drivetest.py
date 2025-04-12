a5f398da2593f6b5f59e7a9e3a33cfb6fbb31b3b
bohua12 <bohua12@gmail.com>
2025-04-12 10:08:22 +0800

    Update drivetest.py

--- drivetest.py
+++ drivetest.py
@@ -1,13 +1,4 @@
-from google.colab import drive
-drive.mount('/content/drive')
-import shutil
-import os
-
+# drivetest.py
 with open("testdrive.txt", "w") as f:
-    f.write("Hello test 1")
-    f.write("Hello test 2")
-
-drive_path = "/content/drive/collabfiles"
-os.makedirs(drive_path, exist_ok=True)
-shutil.move("testdrive.txt", os.path.join(drive_path, "testdrive.txt"))
-print("File saved to Google Drive.")
+    f.write("This file was created by drivetest.py\n")
+print("testdrive.txt created in SASRec.pytorch folder.")
