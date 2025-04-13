5fb58c1639ed160c04180d2fbe2e7691be909fa2
bohua12 <bohua12@gmail.com>
2025-04-13 14:49:22 +0800

    improved logging and print

--- main_new.py
+++ main_new.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
             
             ## Save model only if either of the Validation Metrics improve (Not training Metrics)
             avg_NDCG = (m_valid[0] + a_valid[0] + b_valid[0]) / 3
-            print(f"Epoch {epoch}, Avg NDCG: {avg_NDCG:.5f}", end="")
+            print(f"Epoch {epoch}, Avg NDCG: {avg_NDCG:.5f}, LR: {trainer.adam_optimizer.param_groups[0]["lr"]}", end="")
             f.write(f"Epoch {epoch}, Avg NDCG: {avg_NDCG:.5f} ",)
             if m_valid[0] > best_m_valid_ndcg or a_valid[0] > best_a_valid_ndcg or b_valid[0] > best_b_valid_ndcg:
                 best_m_valid_ndcg = max(m_valid[0], best_m_valid_ndcg)
